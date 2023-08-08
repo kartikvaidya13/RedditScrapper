db31ca9d5dd3bcd4b49bcaffb0362b894590f841
Kartik <kartikvaidya.52@gmail.com>
2023-08-08 15:43:46 -0400

    removed unnecessary files

--- Reddit/__init__.py
+++
@@ -1,9 +0,0 @@
-import praw
-from .reddit_scraper import RedditScraper
-
-
-# Authenticate with Reddit API using praw.ini
-reddit = praw.Reddit("ytbot")
-
-# Initialize RedditScraper instance 
-scraper = RedditScraper(reddit)
--- Reddit/string_filter.py
+++
@@ -1,3 +0,0 @@
-class StringFilter:
-    def __init__(self, str):
-        self.str = str
